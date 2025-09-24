de27b72035f2a764c2ea88142d54d21c40145187
poojasingh5555 <singhpoojareckofficial@gmail.com>
2025-09-24 15:39:59 +0530

    initial first

--- food/vite.config.js
+++ food/vite.config.js
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-   build: {
-    outDir: 'dist',       // default output directory
-    sourcemap: true       // optional, generates source maps
-  }
+  
 })
