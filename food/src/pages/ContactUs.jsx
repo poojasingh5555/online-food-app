6fee0a39d8ab700d68d5b8ad7e09c0350cd32a6d
poojasingh5555 <159644681+poojasingh5555@users.noreply.github.com>
2026-01-16 19:25:12 +0530

    Update ContactUs.jsx

--- food/src/pages/ContactUs.jsx
+++ food/src/pages/ContactUs.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://online-food-app-csne.onrender.com");
 
 export default function ContactUs({ userId }) {
   const [message, setMessage] = useState("");
