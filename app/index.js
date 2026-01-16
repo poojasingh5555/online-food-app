83660a04402d890bcc4e5fab9e5b69e101cb8aea
poojasingh5555 <159644681+poojasingh5555@users.noreply.github.com>
2026-01-16 19:28:31 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 // Attach socket.io
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://online-food-app-csne.onrender.com",
     methods: ["GET", "POST"],
   },
 });
